a33b7e4662dee8cc5e471914391f2f00b33195f6
Wesley <wesley100002001@gmail.com>
2017-03-28 15:29:32 +0800

    Add vue methods example

--- src/app.js
+++ src/app.js
@@ -2,5 +2,10 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'You loaded this page on ' + new Date()
+  },
+  methods: {
+    sendTestRequest: () => {
+      alert('Hello ' + this.name + '!');
+    }
   }
 });
